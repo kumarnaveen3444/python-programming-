9aaca647c234469ea93dc6acac5bd25e590e2234
kumar-naveen <naveenyadav34444@gmail.com>
2026-07-26 21:00:26 +0530

    while condition

---
+++ while.py
@@ -0,0 +1,10 @@
+count = 1
+
+while count <= 5:
+    if count % 2 == 0:
+        
+        print(f"Count is {count} - Even")
+    else:
+        print(f"Count is {count} - Odd")
+    
+    count += 1
